0f95e54cd857bdf225dafff73525aee5402669b0
Akshay Kumar <56194181+eka-lark@users.noreply.github.com>
2021-08-05 14:40:05 +0530

    implements reverse()

--- js-learning-8.js
+++ js-learning-8.js
@@ -1,9 +1,13 @@
 var sentence = 'Bangalore is the best Indian City'
 //output = 'City Indian best the is Bangalore'
-var len = sentence.length;
-var dsentence = sentence.split(" ");
-var j=0;
-var arr =new Array;
-for(var i = dsentence.length - 1; i >= 0; i--)
-    arr[j++] = dsentence[i];
-    console.log(arr)
+
+
+// var len = sentence.length;
+// var dsentence = sentence.split(" ");
+// var j=0;
+// var arr =new Array;
+// for(var i = dsentence.length - 1; i >= 0; i--)
+//     arr[j++] = dsentence[i];
+//     console.log(arr.toString())
+
+console.log(sentence.split(' ').reverse().join(' '));
